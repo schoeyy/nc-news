c1840500e8f314c8ed6a65379e469ea51a675a31
Simba <31388754+schoeyy@users.noreply.github.com>
2023-05-11 12:11:23 +0100

    added 6

--- controllers/nc-news.controller.js
+++ controllers/nc-news.controller.js
@@ -1,8 +1,10 @@
 const {
   fetchNewsTopics,
   fetchArticleById,
+  fetchArticleCommentsById,
 } = require("../models/nc-news.model");
 const apiEndpoints = require("../endpoints.json");
+const comments = require("../db/data/test-data/comments");
 
 exports.getNewsTopics = (req, res, next) => {
   fetchNewsTopics()
@@ -19,6 +21,17 @@ exports.getArticleById = (req, res, next) => {
   fetchArticleById(article_id)
     .then((article) => {
       res.status(200).send({ article });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getArticleCommentsById = (req, res, next) => {
+  const { article_id } = req.params;
+  fetchArticleCommentsById(article_id)
+    .then((comments) => {
+      res.status(200).then({ comments });
     })
     .catch((err) => {
       next(err);
--- models/nc-news.model.js
+++ models/nc-news.model.js
@@ -20,3 +20,24 @@ exports.fetchArticleById = (article_id) => {
       }
     });
 };
+
+exports.fetchArticleCommentsById = (article_id) => {
+  return db
+    .query(
+      `
+    SELECT * FROM comments
+    WHERE article_id = $1
+  `,
+      [article_id]
+    )
+    .then((result) => {
+      if (result.rowCount == 0) {
+        return Promise.reject({
+          code: 404,
+          msg: `Not Found: Article ${article_id} cannot be found!`,
+        });
+      } else {
+        return result.rows;
+      }
+    });
+};

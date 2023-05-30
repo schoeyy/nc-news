63be38f58b9da321a3567aea0248aa771185848a
Simba <31388754+schoeyy@users.noreply.github.com>
2023-05-30 10:46:58 +0100

    allow cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const apiRouter = require("./routes/api.router");
+const cors = require("cors");
 const { customErr, dbErr, errLog } = require("./error-handler");
 
+app.use(cors());
 const app = express();
 app.use(express.json());
 
